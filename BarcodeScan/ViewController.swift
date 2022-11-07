dcafd5feec80544340f1e6320a4b9591ee34fdbe
rivaldofez <rivaldofez@gmail.com>
2022-11-07 22:18:12 +0700

    fixing input and barcode recognizing

--- BarcodeScan/ViewController.swift
+++ BarcodeScan/ViewController.swift
@@ -9,54 +9,36 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
-    var captureSession: AVCaptureSession!
-    var previewLayer: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-            guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
-                print("Devices not support video processing")
-                return
-            }
-            
-            let videoInput: AVCaptureDeviceInput
-            
-            do {
-                videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-            }catch{
-                print("Devices not give video input data")
-                return
-            }
-            
-            if (self.captureSession.canAddInput(videoInput)){
-                self.captureSession.addInput(videoInput)
-            }else{
-                print("Devices cannot add input in capture session")
-                return
-            }
-            
-            let metadataOutput = AVCaptureMetadataOutput()
-            
-            if(self.captureSession.canAddOutput(metadataOutput)){
-                self.captureSession.addOutput(metadataOutput)
-                
-                metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
-            }else{
-                return
-            }
-            
-            
-            self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-            self.previewLayer.frame = self.view.layer.bounds
-            self.previewLayer.videoGravity = .resizeAspectFill
-            self.view.layer.addSublayer(self.previewLayer)
-            print("Running")
-            self.captureSession.startRunning()
-        })
+        let captureSession = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+
+        captureSession.addInput(input)
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if(captureSession.canAddOutput(metadataOutput)){
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+        }else{
+            return
+        }
+        
+        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        
+        captureSession.startRunning()
+
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
     }
 }
 
@@ -69,14 +51,14 @@ extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
             guard let stringValue = readableObject.stringValue else {
                 return
             }
-            
+
             found(code: stringValue)
-            
+
         }else{
             print("Not able to read the code! Please try again")
         }
     }
-    
+
     func found(code: String){
         print(code)
     }
